075591f98d0518b4046a307bed3a7ec8f6d2103f
Marc K <marc@Marcs-MacBook-Air.local>
2021-01-29 17:35:01 +0100

    corrected path for img and css file

--- js/weatherAPIcalls.js
+++ js/weatherAPIcalls.js
@@ -15,32 +15,32 @@ function stateChange() {
 
     if (dropdownValue === 'schluchsee') {
         URL = 'https://api.climacell.co/v3/weather/realtime?lat=47.798916919872866&lon=8.183037307776177&unit_system=si&fields=temp%2Cprecipitation_type&apikey=me2Qm6ZYZ7V8CUhZ5FopsDhQdsqmeV6a'
-        regionImage = "../img/schluchsee-blick.jpg"
+        regionImage = "img/schluchsee-blick.jpg"
     }
 
     if (dropdownValue === 'schauinsland') {
         URL = 'https://api.climacell.co/v3/weather/realtime?lat=47.91368287150694&lon=7.898325385069348&unit_system=si&fields=temp%2Cprecipitation_type&apikey=me2Qm6ZYZ7V8CUhZ5FopsDhQdsqmeV6a'
-        regionImage = "../img/schauinsland.jpg"
+        regionImage = "img/schauinsland.jpg"
     }
 
     if (dropdownValue === 'todtmoos') {
         URL = 'https://api.climacell.co/v3/weather/realtime?lat=47.7416329953509&lon=8.004721615344211&unit_system=si&fields=temp%2Cprecipitation_type&apikey=me2Qm6ZYZ7V8CUhZ5FopsDhQdsqmeV6a'
-        regionImage = "../img/todtmoos.jpg"
+        regionImage = "img/todtmoos.jpg"
     }
 
     if (dropdownValue === 'sustenpass') {
         URL = 'https://api.climacell.co/v3/weather/realtime?lat=46.733239716311395&lon=8.432407803492424&unit_system=si&fields=temp%2Cprecipitation_type&apikey=me2Qm6ZYZ7V8CUhZ5FopsDhQdsqmeV6a'
-        regionImage = "../img/sustenpass.jpg";
+        regionImage = "img/sustenpass.jpg";
     }
 
     if (dropdownValue === 'grimselpass') {
         URL = 'https://api.climacell.co/v3/weather/realtime?lat=46.56157252905508&lon=8.33759132470081&unit_system=si&fields=temp%2Cprecipitation_type&apikey=me2Qm6ZYZ7V8CUhZ5FopsDhQdsqmeV6a'
-        regionImage = '../img/grimselpass.jpg';
+        regionImage = 'img/grimselpass.jpg';
     }
 
     if (dropdownValue === 'gotthardpass') {
         URL = 'https://api.climacell.co/v3/weather/realtime?lat=46.55909324107193&lon=8.562292073220776&unit_system=si&fields=temp%2Cprecipitation_type&apikey=me2Qm6ZYZ7V8CUhZ5FopsDhQdsqmeV6a'
-        regionImage = '../img/gotthardpass.jpg'
+        regionImage = 'img/gotthardpass.jpg'
     }
 
 
